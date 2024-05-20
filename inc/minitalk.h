3d2d32eceaf827492e4134c1cf976ed856e9b429
Zigouingouin <61563425+ft-Ali@users.noreply.github.com>
2024-05-20 16:19:07 +0200

    Add files via upload

---
+++ inc/minitalk.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alsiavos <alsiavos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 13:37:14 by alsiavos          #+#    #+#             */
+/*   Updated: 2024/05/16 18:03:16 by alsiavos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../.libft/printf/ft_printf.h"
+# include "../.libft/libft.h"
+# include <signal.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# define END_TRANSMISSION '\0'
+
+void	ft_handler(int signal, siginfo_t *info, void *context);
+void	ft_signal(int pid, unsigned char character);
+void	signal_handler(int signum, siginfo_t *siginfo, void *context);
+
+#endif
